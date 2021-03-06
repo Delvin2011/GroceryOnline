8047bf5065da1acce25e5b60f9684cf420d00669
Ben Tapfuma <takutapfu@gmail.com>
2021-03-07 00:05:51 +0200

    Add/Loading/Done on cart and sign in/up required on adding to cart

--- client/src/components/Shop/Collection/collection-item.jsx
+++ client/src/components/Shop/Collection/collection-item.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
 // nodejs library that concatenates classes
 import classnames from "classnames";
 import { Link } from "react-router-dom";
 //import Login from "../IndexSections/Login.js";
 // reactstrap components
-
+import { selectCartItemsCount } from "../../../redux/cart/cart-selectors";
+import { createStructuredSelector } from "reselect";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { addItem } from "../../../redux/cart/cart-actions";
+import { selectCurrentUser } from "../../../redux/user/user-selectors";
+import SignIn from "../../../views/examples/SignIn";
+import SignUp from "../../../views/examples/SignUp";
 
 import {
   Badge,
   Button,
   Card,
   CardBody,
-  CardImg, //,
+  CardImg,
+  Modal, //,
   /* FormGroup,
   Input,
   InputGroupAddon,
@@ -25,52 +30,204 @@ import {
   Col*/
 } from "reactstrap";
 
-const CollectionItem = ({ item, addItem, history }) => {
+const CollectionItem = ({ item, addItem, history, itemCount, currentUser }) => {
   const { name, price, imageUrl } = item;
+  const [timeLeft, setTimeLeft] = useState(null);
+  const [modal, setModal] = useState(false);
+  //const [modal, setSignInModal] = useState(false);
+  //const [modal, setSignUpModal] = useState(false);
+  const [signIn, setSignIn] = useState(false);
+  const [signUp, setSignUp] = useState(false);
+  const toggle = () => setModal(!modal);
+  //const toggleSignIn = () => setSignInModal(!modal);
+  //const toggleSignUp = () => setSignUpModal(!modal);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      setTimeLeft(null);
+    }
+    // exit early when we reach 0
+    if (!timeLeft) return;
+    // save intervalId to clear the interval when the
+    // component re-renders
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+      console.log("TIME LEFT IS {0}", timeLeft);
+    }, 1000);
+
+    // clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+    // add timeLeft as a dependency to re-rerun the effect
+    // when we update it
+  }, [timeLeft]);
+
   return (
-    <Card
-      className="card-lift--hover shadow border-0"
-      style={{ backgroundColor: "transparent" }}
-      onClick={() => {
-        history.push("/product-page", item);
-      }}
-    >
-      <CardBody className="py-3 px-3">
-        <Link>
-          <CardImg alt="..." src={imageUrl} />
-        </Link>
-        <div className="mt-2">
-          <Badge color="success" pill className="mr-1 text-capitalize">
-            {name}
-          </Badge>
-        </div>
-        <div>
-          <Badge
-            color="success"
-            pill
-            className="mr-1 lg mt-2"
-            style={{ fontSize: "14px" }}
-          >
-            R {price.toFixed(2)}
-          </Badge>
-        </div>
-        <div className="mt-2">
-          <Button color="primary" onClick={() => addItem(item)}>
-            Add
-          </Button>
-          <Button
-            color="primary"
+    <div>
+      <Card
+        className="card-lift--hover shadow border-0"
+        style={{ backgroundColor: "transparent" }}
+      >
+        <CardBody className="py-3 px-3">
+          <a
             onClick={() => {
               history.push("/product-page", item);
             }}
+            style={{ cursor: "pointer" }}
           >
-            View
-          </Button>
-        </div>
-      </CardBody>
-    </Card>
+            <CardImg alt="..." src={imageUrl} />
+          </a>
+          <div className="mt-2">
+            <Badge color="success" pill className="mr-1 text-capitalize">
+              {name}
+            </Badge>
+          </div>
+          <div>
+            <Badge
+              color="success"
+              pill
+              className="mr-1 lg mt-2"
+              style={{ fontSize: "14px" }}
+            >
+              R {price.toFixed(2)}
+            </Badge>
+          </div>
+          {currentUser ? (
+            <div className="mt-2">
+              {timeLeft >= 3 && timeLeft < 6 ? (
+                <Button color="primary">Loading...</Button>
+              ) : timeLeft < 3 && timeLeft > 0 ? (
+                <Button color="primary">Done...</Button>
+              ) : (
+                <Button
+                  color="primary"
+                  onClick={() => {
+                    addItem(item);
+                    setTimeLeft(5);
+                  }}
+                >
+                  Add
+                </Button>
+              )}
+
+              {/*<Button color="primary" onClick={() => addItem(item)}>
+            Add
+          </Button>*/}
+              <Button
+                color="primary"
+                onClick={() => {
+                  history.push("/product-page", item);
+                }}
+              >
+                View
+              </Button>
+            </div>
+          ) : (
+            <div className="mt-2">
+              <Button color="primary" onClick={toggle}>
+                Add
+              </Button>
+              <Button
+                color="primary"
+                onClick={() => {
+                  history.push("/product-page", item);
+                }}
+              >
+                View
+              </Button>
+              <Modal
+                className="modal-dialog-centered modal-danger"
+                contentClassName="bg-gradient-danger"
+                isOpen={modal}
+                toggle={toggle}
+              >
+                <div className="modal-header">
+                  <h5 className="modal-title" id="modal-title-notification">
+                    Sign In or Sign Up for :
+                  </h5>
+                  <button
+                    aria-label="Close"
+                    className="close"
+                    data-dismiss="modal"
+                    type="button"
+                    onClick={toggle}
+                  >
+                    <span aria-hidden={true}>×</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <div className="py-3 text-left">
+                    <ul>
+                      <li>Customised & smart shopping.</li>
+                      <li>Discount Coupons.</li>
+                      <li>Safe transactions keeping.</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <Button
+                    className="btn-white"
+                    color="default"
+                    type="button"
+                    onClick={() => {
+                      setSignIn(!signIn);
+                      setModal(false);
+                    }}
+                  >
+                    Sign In
+                  </Button>
+                  <Button
+                    className="btn-white"
+                    color="default"
+                    type="button"
+                    onClick={() => {
+                      setSignUp(!signUp);
+                      setModal(false);
+                    }}
+                  >
+                    Sign Up
+                  </Button>
+                  <Button
+                    className="text-white ml-auto"
+                    color="link"
+                    data-dismiss="modal"
+                    type="button"
+                    onClick={toggle}
+                  >
+                    Close
+                  </Button>
+                </div>
+              </Modal>
+            </div>
+          )}
+        </CardBody>
+      </Card>
+      {signIn ? (
+        <Modal
+          className="modal-dialog-centered"
+          size="sm"
+          isOpen={signIn}
+          toggle={() => setSignIn(!signIn)}
+        >
+          <SignIn />
+        </Modal>
+      ) : signUp ? (
+        <Modal
+          className="modal-dialog-centered"
+          size="sm"
+          isOpen={signUp}
+          toggle={() => setSignUp(!signUp)}
+        >
+          <SignIn />
+        </Modal>
+      ) : null}
+    </div>
   );
 };
+
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount,
+  currentUser: selectCurrentUser,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   addItem: (item) => dispatch(addItem(item)),
@@ -78,7 +235,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 export default withRouter(
   connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(CollectionItem)
 );
